79332ef4d2668ee3dc883ca48ade812202c95022
Belal mohsen <belalmohsen113@gmail.com>
2024-04-19 20:56:48 +0200

    AdminMode

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "admin.h"
+
+
+int main(){
+    int mode;
+
+    printf("Welcome to Student Record System\n");
+    printf("Select mode:\n");
+    printf("1.Admin mode\n");
+    printf("2.User mode\n");
+    scanf("%d", &mode);
+
+    if (mode == 1) {
+        adminmode();
+    } else if (mode == 2) {
+       
+    } else {
+        printf("Invalid ' Exit'\n");
+    }
+
+    return 0;
+}
