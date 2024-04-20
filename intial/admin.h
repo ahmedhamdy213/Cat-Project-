8dd60d0531ff209e7f962566afaa6c19f307e8ce
Elhosseny30 <kareem.elhosseny57@gmail.com>
2024-04-20 13:32:49 +0200

    remove

--- intial/admin.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ADMIN_
-#define _ADMIN_
-
-#include "datastructure.h"
-
-void addstudent(Point_t *pl, char name[], float record, int id, int age, char gender[], char pass[]);
-student * get_data_student();
-
-#endif
